49936c5d690519d314cd211299cf9524244eccf3
Brubi25 <148341586+Brubi25@users.noreply.github.com>
2025-02-05 21:23:04 +0100

    aufg 9

---
+++ uebung_09/script.js
@@ -0,0 +1,72 @@
+let form = document.getElementById("form");
+let inputs = form.getElementsByTagName("input");
+
+document.getElementById("submit").onclick = (event) => {
+    event.preventDefault();
+    const map = new Map();
+    for(let i = 0; i < inputs.length; i++){
+        removeHighlight(inputs[i]);
+        if(inputs[i].type == "checkbox"){
+            map.set(inputs[i].id, inputs[i].checked);
+        }else{
+            map.set(inputs[i].id, inputs[i].value);   
+        }
+    }
+    if(!checkValues(map)){
+        let msg = "Werte: \n";
+        map.keys().forEach(element => {
+            msg += element + ": " + map.get(element) + "\n";
+        });
+        alert(msg);
+    }
+}
+
+function removeHighlight(el){
+    el.style.borderColor = "";
+}
+
+function highlightProblem(id, nachricht){
+    let el = document.getElementById(id);
+    el.style.borderColor = "red";
+    el.value = "";
+    el.placeholder = nachricht;
+}
+
+function checkValues(map){
+    let problem = false;
+    if(map.get("name") == ""){
+        highlightProblem("name", "Plichtfeld");
+        problem = true;
+    }
+    let regex = /[\w\-\.]+@([\w-]+\.)+[\w-]{2,}/;
+    if(!regex.test(map.get("email"))){
+        highlightProblem("email", "Falsches Format");
+        problem = true;
+    }
+
+    if(map.get("pw1").length < 8){
+        highlightProblem("pw1", "Passwort zu Kurz");
+        problem = true;
+    }
+
+    if(map.get("pw1") != map.get("pw2")){
+        highlightProblem("pw2", "Passwörter stimmen nicht überein");
+        problem = true;
+    }
+
+    if(map.get("bdate") == ""){
+        highlightProblem("bdate", "Plichtfeld");
+        problem = true;
+    }
+
+    if(map.get("land") == ""){
+        highlightProblem("land", "Plichtfeld");
+        problem = true;
+    }
+
+    if(map.get("condition") === false){
+        highlightProblem("condition", "");
+        problem = true;
+    }
+    return problem;
+}
